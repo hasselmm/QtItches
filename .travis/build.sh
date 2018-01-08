ee5d7ced40642959b2c2de3157c87a92ba707067
Mathias Hasselmann <mathias@taschenorakel.de>
2018-01-08 01:25:06 +0100

    Build each commit of a pull request

---
+++ .travis/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+
+mkdir build
+pushd build
+
+"../5.10.0/${QT_TOOLCHAIN}/bin/qmake" ${QMAKE_FLAGS} ..
+
+popd
+
+if [ "${TRAVIS_PULL_REQUEST}" == "no" ]
+then
+    echo "Building top-level commit for branch ${TRAVIS_BRANCH}..."
+    for target in ${MAKE_TARGETS}; do make -j2 -C build ${target}; done
+else
+    echo "Building each commit in pull request for branch ${TRAVIS_BRANCH}..."
+
+    git describe --all
+    git rebase --exec "for target in qmake_all ${MAKE_TARGETS}; do make -j2 -C build ${target}; done" "${TRAVIS_BRANCH}" HEAD
+fi
+
